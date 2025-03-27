abf1817ab1835dfe55df08bcfe3c4441237e4c10
Surajit Singha Sisir <sci.surajit@gmail.com>
2025-03-28 00:17:45 +0600

    About Page added
    Signed-off-by: Surajit Singha Sisir <sci.surajit@gmail.com>

--- pages/about.vue
+++ pages/about.vue
@@ -18,91 +18,101 @@
                     <NuxtLink to="/videos" class="btn btn-warning">Explore My Works !</NuxtLink>
                 </div>
             </div>
-            <div class="f gap-10">
+            <div class="about-image">
                 <NuxtImg class="img" src="images/about.png" />
             </div>
         </section>
         <section class="gear">
-            <div class="single-gear ">
-                <div class="img-gear">
-                    <div class="overlay-gear">Camera</div>
-                    <NuxtImg src="images/camera.png" />
-                </div>
+            <section class="inner-gear">
+                <div class="single-gear">
+                    <div class="img-gear">
+                        <div class="overlay-gear">Camera</div>
+                        <NuxtImg src="images/camera.png" />
+                    </div>
 
-                <div class="pad--05 border-1 bordered-solid">
-                    <p class="text-dotted-2">This is my description dsafasdfasdf dasfasdfa sfadsf asdf afd asdf adsf </p>
+                    <div class="pad--05 b-Gray-200 b-lrb--01">
+                        <p class="text-dotted-2">This is my description dsafasdfasdf dasfasdfa sfadsf asdf afd asdf adsf
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="single-gear ">
-                <div class="img-gear">
-                    <div class="overlay-gear">Camera</div>
-                    <NuxtImg src="images/camera.png" />
-                </div>
+                <div class="single-gear ">
+                    <div class="img-gear">
+                        <div class="overlay-gear">Camera</div>
+                        <NuxtImg src="images/camera.png" />
+                    </div>
 
-                <div class="pad--05 border-1 bordered-solid">
-                    <p class="text-dotted-2">This is my description dsafasdfasdf dasfasdfa sfadsf asdf afd asdf adsf </p>
+                    <div class="pad--05 b-Gray-200 b-lrb--01">
+                        <p class="text-dotted-2">This is my description dsafasdfasdf dasfasdfa sfadsf asdf afd asdf adsf
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="single-gear ">
-                <div class="img-gear">
-                    <div class="overlay-gear">Camera</div>
-                    <NuxtImg src="images/camera.png" />
-                </div>
+                <div class="single-gear ">
+                    <div class="img-gear">
+                        <div class="overlay-gear">Camera</div>
+                        <NuxtImg src="images/camera.png" />
+                    </div>
 
-                <div class="pad--05 border-1 bordered-solid">
-                    <p class="text-dotted-2">This is my description dsafasdfasdf dasfasdfa sfadsf asdf afd asdf adsf </p>
+                    <div class="pad--05 b-Gray-200 b-lrb--01">
+                        <p class="text-dotted-2">This is my description dsafasdfasdf dasfasdfa sfadsf asdf afd asdf adsf
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="single-gear ">
-                <div class="img-gear">
-                    <div class="overlay-gear">Camera</div>
-                    <NuxtImg src="images/camera.png" />
-                </div>
+                <div class="single-gear ">
+                    <div class="img-gear">
+                        <div class="overlay-gear">Camera</div>
+                        <NuxtImg src="images/camera.png" />
+                    </div>
 
-                <div class="pad--05 border-1 bordered-solid">
-                    <p class="text-dotted-2">This is my description dsafasdfasdf dasfasdfa sfadsf asdf afd asdf adsf </p>
+                    <div class="pad--05 b-Gray-200 b-lrb--01">
+                        <p class="text-dotted-2">This is my description dsafasdfasdf dasfasdfa sfadsf asdf afd asdf adsf
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="single-gear ">
-                <div class="img-gear">
-                    <div class="overlay-gear">Camera</div>
-                    <NuxtImg src="images/camera.png" />
-                </div>
+                <div class="single-gear ">
+                    <div class="img-gear">
+                        <div class="overlay-gear">Camera</div>
+                        <NuxtImg src="images/camera.png" />
+                    </div>
 
-                <div class="pad--05 border-1 bordered-solid">
-                    <p class="text-dotted-2">This is my description dsafasdfasdf dasfasdfa sfadsf asdf afd asdf adsf </p>
+                    <div class="pad--05 b-Gray-200 b-lrb--01">
+                        <p class="text-dotted-2">This is my description dsafasdfasdf dasfasdfa sfadsf asdf afd asdf adsf
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="single-gear ">
-                <div class="img-gear">
-                    <div class="overlay-gear">Camera</div>
-                    <NuxtImg src="images/camera.png" />
-                </div>
+                <div class="single-gear ">
+                    <div class="img-gear">
+                        <div class="overlay-gear">Camera</div>
+                        <NuxtImg src="images/camera.png" />
+                    </div>
 
-                <div class="pad--05 border-1 bordered-solid">
-                    <p class="text-dotted-2">This is my description dsafasdfasdf dasfasdfa sfadsf asdf afd asdf adsf </p>
+                    <div class="pad--05 b-Gray-200 b-lrb--01">
+                        <p class="text-dotted-2">This is my description dsafasdfasdf dasfasdfa sfadsf asdf afd asdf adsf
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="single-gear ">
-                <div class="img-gear">
-                    <div class="overlay-gear">Camera</div>
-                    <NuxtImg src="images/camera.png" />
-                </div>
+                <div class="single-gear ">
+                    <div class="img-gear">
+                        <div class="overlay-gear">Camera</div>
+                        <NuxtImg src="images/camera.png" />
+                    </div>
 
-                <div class="pad--05 border-1 bordered-solid">
-                    <p class="text-dotted-2">This is my description dsafasdfasdf dasfasdfa sfadsf asdf afd asdf adsf </p>
+                    <div class="pad--05 b-Gray-200 b-lrb--01">
+                        <p class="text-dotted-2">This is my description dsafasdfasdf dasfasdfa sfadsf asdf afd asdf adsf
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="single-gear ">
-                <div class="img-gear">
-                    <div class="overlay-gear">Camera</div>
-                    <NuxtImg src="images/camera.png" />
-                </div>
+                <div class="single-gear ">
+                    <div class="img-gear">
+                        <div class="overlay-gear">Camera</div>
+                        <NuxtImg src="images/camera.png" />
+                    </div>
 
-                <div class="pad--05 border-1 bordered-solid ">
-                    <p class="text-dotted-2">This is my description dsafasdfasdf dasfasdfa sfadsf asdf afd asdf adsf </p>
+                    <div class="pad--05 b-Gray-200 b-lrb--01">
+                        <p class="text-dotted-2">This is my description dsafasdfasdf dasfasdfa sfadsf asdf afd asdf adsf
+                        </p>
+                    </div>
                 </div>
-            </div>
+            </section>
         </section>
 
     </section>
@@ -113,43 +123,83 @@
     position: relative;
 }
 
-.gear {
-    width: 90%;
-    height: auto;
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    gap: 1rem;
-    margin: auto;
+.about-image {
     position: relative;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+}
 
-    .single-gear {
-        position: inherit;
+.gear {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+
+    .inner-gear {
+        width: 70%;
+        height: auto;
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        gap: 1rem;
         position: relative;
+        padding: 2rem 0;
 
-        .overlay-gear {
-            position: absolute;
-            padding: 0.2rem 0.5rem;
-            right: 0;
-            top: 0;
-            border-top-left-radius: 0.5rem;
-            border-bottom-left-radius: 0.5rem;
-            font-size: 0.8rem;
-            color: white;
-            background-color: green;
+        @media screen and (max-width: 1080px) {
+            grid-template-columns: repeat(4, 1fr);
         }
 
-        img {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-
+        @media screen and (max-width: 768px) {
+            grid-template-columns: repeat(3, 1fr);
         }
 
-        .img-gear {
-            position:relative;
-            border: 1px solid #ccc;
+        @media screen and (max-width: 520px) {
+            grid-template-columns: repeat(2, 1fr);
+        }
+
+        @media screen and (max-width: 320px) {
+            grid-template-columns: repeat(1, 1fr);
+        }
+
+        .single-gear {
+            position: inherit;
+            position: relative;
+            z-index: 5;
+
+            .overlay-gear {
+                position: absolute;
+                padding: 0.2rem 0.5rem;
+                right: 0;
+                top: 0;
+                border-top-left-radius: 0.5rem;
+                border-bottom-left-radius: 0.5rem;
+                font-size: 0.8rem;
+                color: white;
+                background-color: green;
+                z-index: 5;
+            }
+
+            .img-gear {
+                position: relative;
+                width: 100%;
+                aspect-ratio: 1;
+                border: 1px solid #ccc;
+                overflow: hidden;
+
+                img {
+                    width: 100%;
+                    height: 100%;
+                    object-fit: cover;
+                    transition: all 0.3s ease;
+
+                    &:hover {
+                        transform: scale(1.1);
+                        transition: all 0.3s ease;
+                    }
+
+                }
+            }
         }
     }
-
 }
 </style>
